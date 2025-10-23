bc5cf309969c3c1aeaaf657003259a1f7a6a6c82
mohd191mohsin <mohd191mohsin@gmail.com>
2025-10-23 22:52:39 +0530

    test

---
+++ docs/js/script.js
@@ -0,0 +1,7 @@
+
+  fetch('sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('sidebar-placeholder').innerHTML = data;
+    })
+    .catch(err => console.error('Error loading sidebar:', err));
